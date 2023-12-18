4c38cafe059d6cf8ad69b4ea2d2aaaca3ba73ae9
hamiddeboo8 <hamiddeh19@gmail.com>
2023-12-18 21:53:56 +0330

    increase coverage of personRepo

--- CodeCoverageProject/src/test/java/com/unittest/codecoverage/newTest/PersonRepoTest.java
+++ CodeCoverageProject/src/test/java/com/unittest/codecoverage/newTest/PersonRepoTest.java
@@ -1,11 +1,17 @@
 package com.unittest.codecoverage.newTest;
 
+import com.unittest.codecoverage.exceptions.PersonException;
 import com.unittest.codecoverage.models.Gender;
 import com.unittest.codecoverage.models.Person;
 import com.unittest.codecoverage.repositories.PersonRepository;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class PersonRepoTest {
 
@@ -30,5 +36,22 @@ public class PersonRepoTest {
 
         PersonRepository pr = new PersonRepository();
         pr.update(p);
+    }
+
+    @Test
+    public void repoDeleteTest(){
+        Person p = new Person();
+        p.setName(null);
+        p.setAge(71);
+        p.setGender(Gender.F);
+
+        PersonRepository pr = new PersonRepository();
+
+        List<String> expectedErrors = Lists.newArrayList("name can't be null");
+        String expectedMessage = String.join(";", expectedErrors);
+
+        assertThatThrownBy(() -> pr.delete(p.getName()))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage(expectedMessage);
     }
 }

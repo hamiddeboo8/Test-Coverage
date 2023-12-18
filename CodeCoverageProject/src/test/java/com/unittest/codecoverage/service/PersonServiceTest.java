c3d8bf8ba854100b61ff2a09e2f238732818f618
MoNam97 <mohamad0namdar@gmail.com>
2023-12-18 18:21:44 +0330

    improve PersonServiceTest by adding test for service get function + add test for PersonException's String constructor

--- CodeCoverageProject/src/test/java/com/unittest/codecoverage/service/PersonServiceTest.java
+++ CodeCoverageProject/src/test/java/com/unittest/codecoverage/service/PersonServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import java.util.List;
 
 import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,79 +23,91 @@ import com.unittest.codecoverage.services.impl.PersonServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
-	
-	@InjectMocks
-	PersonService service = new PersonServiceImpl();
-	@Mock
-	PersonRepository repository;
-	
-	@Test
-	public void testInsert_shouldInsertPersonWithSuccessWhenAllPersonsInfoIsFilled() {
-		Person person = new Person();
-		person.setName("Name");
-		person.setAge(21);
-		person.setGender(Gender.M);
-		
-		when(repository.insert(any(Person.class))).thenReturn(person);
-		
-		service.insert(person);
-	}
-	
-	@Test
-	public void testInsert_shouldThrowPersonExceptionWhenPersonIsNull() {
-		
-		List<String> expectedErrors = Lists.newArrayList("Name is required", "Gender is required");
-		String expectedMessage = String.join(";", expectedErrors);
-		Person person = null;
-		
-		assertThatThrownBy(() -> service.insert(person))
-			.isInstanceOf(PersonException.class)
-			.hasFieldOrPropertyWithValue("errors", expectedErrors)
-			.hasMessage(expectedMessage);
-	}
-	
-	@Test
-	public void testInsert_shouldThrowPersonExceptionWhenPersonNameIsNull() {
-		
-		List<String> expectedErrors = Lists.newArrayList("Name is required");
-		String expectedMessage = String.join(";", expectedErrors);
-		Person person = new Person();
-		person.setGender(Gender.M);
-		
-		assertThatThrownBy(() -> service.insert(person))
-			.isInstanceOf(PersonException.class)
-			.hasFieldOrPropertyWithValue("errors", expectedErrors)
-			.hasMessage(expectedMessage);
-	}
-	
-	@Test
-	public void testInsert_shouldThrowPersonExceptionWhenPersonNameIsBlank() {
-		
-		List<String> expectedErrors = Lists.newArrayList("Name is required");
-		String expectedMessage = String.join(";", expectedErrors);
-		Person person = new Person();
-		person.setGender(Gender.M);
-		person.setName(" ");
-		
-		assertThatThrownBy(() -> service.insert(person))
-			.isInstanceOf(PersonException.class)
-			.hasFieldOrPropertyWithValue("errors", expectedErrors)
-			.hasMessage(expectedMessage);
-	}
-	
-	@Test
-	public void testInsert_shouldThrowPersonExceptionWhenPersonGenderIsNull() {
-		
-		List<String> expectedErrors = Lists.newArrayList("Gender is required");
-		String expectedMessage = String.join(";", expectedErrors);
-		Person person = new Person();
-		person.setName("Name");
-		person.setGender(null);
-		
-		assertThatThrownBy(() -> service.insert(person))
-			.isInstanceOf(PersonException.class)
-			.hasFieldOrPropertyWithValue("errors", expectedErrors)
-			.hasMessage(expectedMessage);
-	}
+
+    @InjectMocks
+    PersonService service = new PersonServiceImpl();
+    @Mock
+    PersonRepository repository;
+
+    @Test
+    public void testInsert_shouldInsertPersonWithSuccessWhenAllPersonsInfoIsFilled() {
+        Person person = new Person();
+        person.setName("Name");
+        person.setAge(21);
+        person.setGender(Gender.M);
+
+        when(repository.insert(any(Person.class))).thenReturn(person);
+
+        service.insert(person);
+    }
+
+    @Test
+    public void testInsert_shouldThrowPersonExceptionWhenPersonIsNull() {
+
+        List<String> expectedErrors = Lists.newArrayList("Name is required", "Gender is required");
+        String expectedMessage = String.join(";", expectedErrors);
+        Person person = null;
+
+        assertThatThrownBy(() -> service.insert(person))
+                .isInstanceOf(PersonException.class)
+                .hasFieldOrPropertyWithValue("errors", expectedErrors)
+                .hasMessage(expectedMessage);
+    }
+
+    @Test
+    public void testInsert_shouldThrowPersonExceptionWhenPersonNameIsNull() {
+
+        List<String> expectedErrors = Lists.newArrayList("Name is required");
+        String expectedMessage = String.join(";", expectedErrors);
+        Person person = new Person();
+        person.setGender(Gender.M);
+
+        assertThatThrownBy(() -> service.insert(person))
+                .isInstanceOf(PersonException.class)
+                .hasFieldOrPropertyWithValue("errors", expectedErrors)
+                .hasMessage(expectedMessage);
+    }
+
+    @Test
+    public void testInsert_shouldThrowPersonExceptionWhenPersonNameIsBlank() {
+
+        List<String> expectedErrors = Lists.newArrayList("Name is required");
+        String expectedMessage = String.join(";", expectedErrors);
+        Person person = new Person();
+        person.setGender(Gender.M);
+        person.setName(" ");
+
+        assertThatThrownBy(() -> service.insert(person))
+                .isInstanceOf(PersonException.class)
+                .hasFieldOrPropertyWithValue("errors", expectedErrors)
+                .hasMessage(expectedMessage);
+    }
+
+    @Test
+    public void testInsert_shouldThrowPersonExceptionWhenPersonGenderIsNull() {
+
+        List<String> expectedErrors = Lists.newArrayList("Gender is required");
+        String expectedMessage = String.join(";", expectedErrors);
+        Person person = new Person();
+        person.setName("Name");
+        person.setGender(null);
+
+        assertThatThrownBy(() -> service.insert(person))
+                .isInstanceOf(PersonException.class)
+                .hasFieldOrPropertyWithValue("errors", expectedErrors)
+                .hasMessage(expectedMessage);
+    }
+
+    @Test
+    @DisplayName("Test Person Exception when Person Name is null")
+    public void testGet_shouldThrowPersonExceptionWhenPersonNameIsNull() {
+
+        String expectedMessage = "Name is required";
+
+        assertThatThrownBy(() -> service.get("Javad"))
+                .isInstanceOf(PersonException.class)
+                .hasMessage(expectedMessage);
+
+    }
 
 }
